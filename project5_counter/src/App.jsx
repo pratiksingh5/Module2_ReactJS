2fe8665e5f769c0566dde5f803e7a9708682acd2
Pratik Singh <pratikrajaryan@gmail.com>
2024-03-16 12:16:43 +0530

    project5_counter

---
+++ project5_counter/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const handleIncrease = () => {
+    setCount(count + 1)
+  }
+
+  const handleDecrease = () => {
+    setCount(count - 1)
+  }
+
+  return (
+    <>
+      <h1>Counter App</h1>
+      <div className="card">
+      <button onClick={handleDecrease}>
+          Remove 
+        </button>
+        <span style={{padding: "10px 50px",  backgroundColor: "#f1f1f1", margin: "0 10px"}}>{count}</span>
+        <button onClick={handleIncrease}>
+          Add 
+        </button>
+
+ 
+      </div>
+    </>
+  )
+}
+
+export default App
